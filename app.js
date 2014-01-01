5dbda205d39c04590c95da90c598e3d92094465f
Tim Sullivan <tsullivan@paypal.com>
2014-01-01 20:29:45 +0000

    added fortune endpoint

--- app.js
+++ app.js
@@ -25,11 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
+app.get('/fortune', routes.fortune);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,31 @@
  */
 
 exports.index = function(req, res){
-  res.render('index.html');
+	res.render('index.html');
 };
+
+/*
+ * GET a fortune.
+ */
+
+exports.fortune = function(req, res){
+	// http://www.dzone.com/snippets/execute-unix-command-nodejs
+	var command = "fortune"
+		, sys = require('sys')
+		, exec = require('child_process').exec
+		, child;
+
+
+	res.setHeader('Content-Type', 'application/json');
+
+	// executes `pwd`
+	child = exec(command, function (error, stdout, stderr) {
+		if (error !== null) {
+			console.log('exec error: ' + error);
+			return Error(error);
+		}
+
+		res.end(JSON.stringify({ message: stdout }));
+	});
+
+}
